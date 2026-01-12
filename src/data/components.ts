5df78b93e5a49cff5a8928912bf47c7403c420a4
Sathit Seethaphon <dixonsatit@gmail.com>
2026-01-12 19:36:35 +0700

    feat(departments): regenerate with hierarchy and add hospital test data

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,6 +1,6 @@
 /**
  * AUTO-GENERATED FILE
- * Generated at: 2026-01-12T05:14:15.772Z
+ * Generated at: 2026-01-12T12:28:51.097Z
  * Generator: components-generator@1.0.0
  * Source files:
  *   - libs/aegisx-ui/src/lib/components/
--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -1,6 +1,6 @@
 /**
  * AUTO-GENERATED FILE
- * Generated at: 2026-01-12T05:14:16.128Z
+ * Generated at: 2026-01-12T12:28:51.114Z
  * Generator: patterns-generator@1.0.0
  * Source files:
  *   - libs/aegisx-mcp/src/data/patterns.ts
