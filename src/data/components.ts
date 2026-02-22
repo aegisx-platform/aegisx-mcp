ad733a55784a3d78d90efc43d60b9617c06d6642
Sathit Seethaphon <dixonsatit@gmail.com>
2026-02-23 06:02:03 +0700

    chore(mcp): sync components and patterns data

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,6 +1,6 @@
 /**
  * AUTO-GENERATED FILE
- * Generated at: 2026-02-22T10:25:58.081Z
+ * Generated at: 2026-02-22T23:00:35.183Z
  * Generator: components-generator@1.0.0
  * Source files:
  *   - libs/aegisx-ui/src/lib/components/
--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -1,6 +1,6 @@
 /**
  * AUTO-GENERATED FILE
- * Generated at: 2026-02-22T10:25:58.600Z
+ * Generated at: 2026-02-22T23:00:35.195Z
  * Generator: patterns-generator@1.0.0
  * Source files:
  *   - libs/aegisx-mcp/src/data/patterns.ts
